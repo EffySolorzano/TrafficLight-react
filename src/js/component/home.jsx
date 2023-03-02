b4fdef622ae398d838f1037d36f3afc4c44085f6
EffySolorzano <solorzano.steph@gmail.com>
2023-03-02 02:05:02 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,13 +6,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="text-center bottom-fixed">
+		<p className="fixed-bottom">
+		  Made by
+		  <a href="https://github.com/EffySolorzano"> Solórzano 
+		  </a><i className="fa fa-feather"></i>
+		  </p>
+		  </div>
 	);
 };
 
